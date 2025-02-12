e479b7e7f150c49dbfcd5926e755ae0e1d00b69f
davidbernson <david.j.bernson@gmail.com>
2025-02-12 08:15:23 +0100

    worked on porblem 3

--- 3/part2.py
+++ 3/part2.py
@@ -3,12 +3,17 @@ import re
 with open("./3/data/data.txt") as file:
     input = file.read()
 
-print(input)
-
-pattern = re.compile(r"mul\(\d+,\d+\)")
+pattern = re.compile(r"(.*)(don\'t\(\))(.*)(do\(\))(.*)")
+pattern_expr = re.compile(r"mul\(\d+,\d+\)")
 pattern_tuples = re.compile(r"\d+")
 
-expressions = pattern.findall(input)
+first_parse = pattern.findall(input)
+
+print(first_parse[0])
+
+expressions = pattern_expr.findall(first_parse)[0]
+
+print(expressions)
 
 result = 0
 
