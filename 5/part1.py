2fbeff844c6731a45b77139a0c797eaf3b9cf668
davidbernson <david.j.bernson@gmail.com>
2025-02-14 16:11:27 +0100

    Solved part b for day 5.

--- 5/part1.py
+++ 5/part1.py
@@ -1,4 +1,4 @@
-with open("./5/data/testdata.txt") as file:
+with open("./5/data/data.txt") as file:
     input = [line for line in file]
 
 #PRE-PROCESSING
@@ -12,12 +12,14 @@ instructions = [line.rstrip() for line in input if ',' in line]
 instructions = [tuple(map(int, instruction.split(sep=','))) for instruction in instructions]
 
 #BUILD ORDERING DICT
-
 def buildDict(instruction):
 
     pageNumbersDict = {key: value for value, key in enumerate(instruction)}
 
     return pageNumbersDict
+
+def buildRuleSubset(instruction, rules):
+    return [rule for rule in rules if rule[0] in list(instruction) and rule[1] in list(instruction)]
 
 #SWAP
 def swap(dict, a, b):
@@ -33,7 +35,7 @@ def swap(dict, a, b):
 #MANIPULATE ORDERING DICT
 def orderDict(pageNumbersDict, rules):
 
-    ruleSubset = [rule for rule in rules if rule[0] in list(pageNumbersDict.keys()) and rule[1] in list(pageNumbersDict.keys())]
+    ruleSubset = buildRuleSubset(pageNumbersDict, rules)
 
     for rule in ruleSubset:
         
@@ -51,8 +53,30 @@ def orderDict(pageNumbersDict, rules):
     # print(pageNumbersDict)
     return pageNumbersDict
 
+#VALIDATE INSTRUCTION
+def validateRule(instruction, rule):
+    instruction = list(instruction)    
+    a = rule[0]
+    b = rule[1]
+    
+    if instruction.index(a) < instruction.index(b):
+        return True
+    else:
+        return False
+
+def validateInstruction(instruction, rules):
+    ruleSubset = buildRuleSubset(instruction, rules)
+
+    for rule in ruleSubset:
+        if validateRule(instruction, rule):
+            continue
+        else:
+            return False
+    return True
+
 def orderInstruction(instruction, rules):
-    ruleSubset = [rule for rule in rules if rule[0] in list(pageNumbersDict.keys()) and rule[1] in list(pageNumbersDict.keys())]
+
+    ruleSubset = buildRuleSubset(instruction, rules)
     
     instruction = list(instruction)
 
@@ -61,14 +85,15 @@ def orderInstruction(instruction, rules):
         a = rule[0]
         b = rule[1]
 
-        if instruction.index(a) > instruction.index(b):
-            print(f'{a} is after {b}, thus violating rule {rule}.')
+        stored_a = instruction.index(a)
+        stored_b = instruction.index(b)
 
-            a, b = instruction.index(a), instruction.index(b)
-            instruction[b], instruction[a] = instruction[a], instruction[b]
+        if not validateRule(instruction, rule):
+            print(f'Rule {rule} broken. Changing order of {a} and {b} in {instruction}')
+            instruction[stored_b], instruction[stored_a] = instruction[stored_a], instruction[stored_b]
+            print(f'New instruction: {instruction}') 
 
     return instruction
-            
 
 #VALIDATING INSTRUCTIONS
 resultValid = 0
@@ -78,27 +103,18 @@ for instruction in instructions:
     pageNumbersDict = buildDict(instruction)
     orderedPageNumbers = orderDict(pageNumbersDict, rules)
 
-    isValidInstruction = False
-    prev_value = -1
-
-    for page in instruction:
-        if orderedPageNumbers[page] > prev_value:
-            prev_value = orderedPageNumbers[page]
-            isValidInstruction = True
-            continue
-        else:
-            isValidInstruction = False
-            break
-
-    if isValidInstruction:    
+    if validateInstruction(instruction, rules):    
         print(f'{instruction} is a valid instruction')
         resultValid += instruction[len(instruction)//2]
     else:
-        print(f'{instruction} is an invalid instruction')
-        instruction = orderInstruction(instruction, rules)
+
+        while not validateInstruction(instruction, rules):
+            instruction = orderInstruction(instruction, rules)
+            
         print(f'Ordered instruction: {instruction}')
         resultInvalid += instruction[len(instruction)//2]
 
+
 print(resultValid, resultInvalid)
 
-    
+     
